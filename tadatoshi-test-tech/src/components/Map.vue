f7f71cd725c9486c1c89cbf12373ab8313c961dd
Miura <tadamiura@gmail.com>
2020-11-24 19:43:57 +0100

    created map with center position on location and fetch bikes position with markers

---
+++ tadatoshi-test-tech/src/components/Map.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <GmapMap
+      :center="{ lat: currentLocation.lat, lng: currentLocation.lng }"
+      :zoom="14"
+      map-type-id="terrain"
+      style="width: 800px; height: 600px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(bike, index) in info"
+        :position="{
+          lat: bike.location.coordinates[1],
+          lng: bike.location.coordinates[0],
+        }"
+        @click="showModal(bike)"
+      />
+    </GmapMap>
+    <Modal
+      v-show="isModalVisible"
+      @close="closeModal"
+      :bikeInformations="bikeInformations"
+    />
+  </div>
+</template>
+<script>
+import axios from "axios";
+import Modal from './Modal'
+export default {
+  components: {
+      Modal,
+  },
+  data() {
+    return {
+      info: null,
+      currentLocation: { lat: 0, lng: 0 },
+      isModalVisible: false,
+      bikeInformations: ''
+    };
+  },
+  created() {
+    axios
+      .get("https://jsonbox.io/box_a3adbf3558b31a69259a")
+      .then((res) =>
+        res.data.forEach((item, index) => {
+          this.info = res.data;
+        })
+      )
+      .catch((error) => console.log('error', error));
+  },
+  mounted() {
+    this.geolocation();
+  },
+  methods: {
+    geolocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.currentLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      });
+    },
+    showModal(bike) {
+        this.isModalVisible = true;
+        this.bikeInformations = bike
+      },
+      closeModal(bike) {
+        this.isModalVisible = false;
+      },
+    // onClick(bike) {
+    //     alert(
+    //         "Détail du vélo : \nstatus " +
+    //         bike.service_status + "\n" +
+    //         "serial number " +
+    //         bike.serial_number + "\n" +
+    //         "battery level " +
+    //         bike.battery_level
+    //     );
+    // },
+  },
+};
+</script>
