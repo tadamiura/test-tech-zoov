d7c2a35b55e31e862cda33ae7f79018a1a6d626f
Miura <tadamiura@gmail.com>
2020-11-24 19:49:49 +0100

    import vue-router

--- tadatoshi-test-tech/src/main.js
+++ tadatoshi-test-tech/src/main.js
@@ -2,8 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 import * as GmapVue from 'gmap-vue'
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 Vue.use(GmapVue, {
   load: {
