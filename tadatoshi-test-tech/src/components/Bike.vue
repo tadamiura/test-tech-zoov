cb9f1f64af2a2ac644dedb73846f1fc8352a0642
Miura <tadamiura@gmail.com>
2020-11-24 10:06:19 +0100

    added center to a current location

--- tadatoshi-test-tech/src/components/Bike.vue
+++ tadatoshi-test-tech/src/components/Bike.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
         <GmapMap
-            :center="{lat:48.7723, lng:2.24356}"
-            :zoom="16"
+            :center="{lat: currentLocation.lat, lng: currentLocation.lng}"
+            :zoom="12"
             map-type-id="terrain"
             style="width: 800px; height: 600px"
         >
             <GmapMarker
-            :key="index"
-            v-for="(bike, index) in markers"
-            :position="bike.position"
-            :clickable="true"
-            :draggable="true"
-            @click="center=bike.position"
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="center=m.position"
             />
         </GmapMap>  
         <div v-for="bike in info" :key="bike.id">
@@ -26,25 +26,30 @@
 </template>
 <script>
 import axios from 'axios'
-// import VueAxios from 'vue-axios'
 export default {
     data () {
         return{
             info: null,
-            center: null,
-            markers: []
+            markers: [],
+            currentLocation : { lat:0, lng: 0 },
         }
     },
     mounted() {
+        this.geolocation();
         axios
         .get("https://jsonbox.io/box_a3adbf3558b31a69259a")
-        .then((res) => (this.info = res.data))
-        .catch(error => console.log(error))
+        .then((res) => (this.info = res.data) && console.log(res.data))
+        .catch(error => console.log(error));
     },
     methods: {
-        fetchBikes: function () {
-            console.log(console.log('bike'))
-        }
+        geolocation () {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.currentLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+                };
+            });
+        },
     }
 };
 </script>
