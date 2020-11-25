0830bfccd9fd41ae06df8f29d9673fbc5268e607
Miura <tadamiura@gmail.com>
2020-11-25 23:11:49 +0100

    use vue router to display Map

--- tadatoshi-test-tech/src/App.vue
+++ tadatoshi-test-tech/src/App.vue
@@ -1,30 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <Map/>
+  <router-view></router-view>  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 import Map from './components/Map'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Map
+    Map,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
