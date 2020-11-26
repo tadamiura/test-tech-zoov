90a1dc6ad87a74d9c979641ebe49ff4094a0eaf3
Miura <tadamiura@gmail.com>
2020-11-26 11:46:57 +0100

    deleted style in the component

--- tadatoshi-test-tech/src/components/Modal.vue
+++ tadatoshi-test-tech/src/components/Modal.vue
@@ -47,7 +47,7 @@
             >
               Modifier 
           </button> -->
-          <router-link :to="{name: 'update-bike'}" 
+          <router-link :to="{name: 'update-bike', params:{bikeId: bikeInformations.serial_number}}" 
           class="update-button" 
           tag="button"
           >
@@ -69,74 +69,13 @@
       close() {
         this.$emit('close');
       },
-      updateStatus(bikeInformations) {
-        this.bikeInformation = bikeInformations
-        this.$route.push({
-          path: '/bike'
-        })
-        this.$emit('close');
-      }
+      // updateStatus(bikeInformations) {
+      //   this.bikeInformation = bikeInformations
+      //   this.$route.push({
+      //     path: '/bike'
+      //   })
+      //   this.$emit('close');
+      // }
     },
   };
 </script>
-<style>
-  .modal-backdrop {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .modal {
-    background: #FFFFFF;
-    box-shadow: 2px 2px 20px 1px;
-    overflow-x: auto;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .modal-header,
-  .modal-footer {
-    padding: 15px;
-    display: flex;
-  }
-
-  .modal-header {
-    border-bottom: 1px solid #eeeeee;
-    color: #4AAE9B;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .modal-footer {
-    border-top: 1px solid #eeeeee;
-    justify-content: space-between;
-  }
-
-  .modal-body {
-    position: relative;
-    padding: 20px 10px;
-  }
-
-  .btn-close {
-    border: none;
-    font-size: 20px;
-    padding: 20px;
-    cursor: pointer;
-    font-weight: bold;
-    color: #4AAE9B;
-    background: transparent;
-  }
-
-  .btn-green {
-    color: white;
-    background: #4AAE9B;
-    border: 1px solid #4AAE9B;
-    border-radius: 2px;
-  }
-</style>
