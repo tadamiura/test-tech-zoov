eb186bfc0a6b436758294851c2f0e867aa250bff
Miura <tadamiura@gmail.com>
2020-11-26 21:18:48 +0100

    indentation improved

--- tadatoshi-test-tech/src/components/Modal.vue
+++ tadatoshi-test-tech/src/components/Modal.vue
@@ -1,17 +1,15 @@
 <template>
   <transition name="modal-fade">
     <div class="modal-backdrop">
-      <div class="modal"
+      <div
+        class="modal"
         role="dialog"
         aria-labelledby="modalTitle"
         aria-describedby="modalDescription"
       >
-        <header
-          class="modal-header"
-          id="modalTitle"
-        >
+        <header class="modal-header" id="modalTitle">
           <slot name="header">
-              Informations sur le vélo
+            Informations sur le vélo
             <button
               type="button"
               class="btn-close"
@@ -22,17 +20,16 @@
             </button>
           </slot>
         </header>
-        <section
-          class="modal-body"
-          id="modalDescription"
-        >
+        <section class="modal-body" id="modalDescription">
           <slot name="body">
-              <ul>
-                  <li>Numéro de série : {{ bikeInformations.serial_number }}</li>
-                  <li>En ordre : {{ bikeInformations.in_order }}</li>
-                  <li>Niveau de la batterie : {{ bikeInformations.battery_level }}/100</li>
-                  <li>status : {{ bikeInformations.service_status }}</li>
-              </ul>
+            <ul>
+              <li>Numéro de série : {{ bikeInformations.serial_number }}</li>
+              <li>En ordre : {{ bikeInformations.in_order }}</li>
+              <li>
+                Niveau de la batterie : {{ bikeInformations.battery_level }}/100
+              </li>
+              <li>status : {{ bikeInformations.service_status }}</li>
+            </ul>
           </slot>
         </section>
       </div>
@@ -40,15 +37,13 @@
   </transition>
 </template>
 <script>
-  export default {
-    name: 'modal',
-    props: [
-        'bikeInformations'
-    ],
-    methods: {
-      close() {
-        this.$emit('close');
-      },
+export default {
+  name: "modal",
+  props: "bikeInformations",
+  methods: {
+    close() {
+      this.$emit("close");
     },
-  };
+  },
+};
 </script>
