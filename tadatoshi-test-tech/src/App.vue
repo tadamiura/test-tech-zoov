b97d0a10b9fc8446c585eb3a4a5a5a7a9923b843
Miura <tadamiura@gmail.com>
2020-11-26 17:16:37 +0100

    imported fontawesome for vue and display icons on footer

--- tadatoshi-test-tech/src/App.vue
+++ tadatoshi-test-tech/src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-  <router-view></router-view>  
+    <header class="header">
+      <img src="./assets/logo.png" alt="logo">
+    </header>
+    <Map />
+    <footer>
+      <div class="contact-us">
+        <a :href="`mailto:${contact}`">Contact us</a>
+      </div>
+      <div class="follow-us-container">
+        <a href=""><font-awesome-icon :icon="['fab', 'facebook']" size="2x" /></a>
+        <a href=""><font-awesome-icon :icon="['fab', 'linkedin']" size="2x"/></a>
+        <a href=""><font-awesome-icon :icon="['fab', 'instagram']" size="2x" /></a>
+        <a href=""><font-awesome-icon :icon="['fab', 'snapchat']" size="2x" /></a>    
+        <a href=""><font-awesome-icon :icon="['fab', 'youtube']" size="2x" /></a>
+        <a href=""><font-awesome-icon :icon="['fab', 'linkedin']" size="2x" /></a>  
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -10,7 +26,12 @@ import Map from './components/Map'
 export default {
   name: 'App',
   components: {
-    Map,
+    Map
+  },
+  data () {
+    return {
+      contact: 'tadamiura@gmail.com'
+    }
   }
 }
 </script>
