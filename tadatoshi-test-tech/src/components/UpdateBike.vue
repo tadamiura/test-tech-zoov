0ef5e4839ac167d93795ecba718598013c612a02
Miura <tadamiura@gmail.com>
2020-11-27 09:52:57 +0100

    organized informations and used method to display more explicite details

--- tadatoshi-test-tech/src/components/UpdateBike.vue
+++ tadatoshi-test-tech/src/components/UpdateBike.vue
@@ -1,13 +1,46 @@
 <template>
     <div class="update-bike">
-        <h2><font-awesome-icon icon="info-circle"/>Bike's Informations</h2>
+        <h2><font-awesome-icon icon="info-circle"/>Selected Bike's Informations</h2>
         <ul>
-            <li><font-awesome-icon icon="bicycle"/>Numéro de série : {{ bikeInformations.serial_number }}</li>
-            <li><font-awesome-icon icon="parking"/>En ordre : {{ bikeInformations.in_order }}</li>
-            <li><font-awesome-icon icon="battery-three-quarters"/>Niveau de batterie : {{ bikeInformations.battery_level}}/{{ maxLevel }}</li>
-            <li><font-awesome-icon icon="compass"/>Coordonnées : <br>
-            lat : {{ bikeInformations.location.coordinates[1]}}<br>
-            lng : {{ bikeInformations.location.coordinates[0]}}</li>
+            <li>
+                <font-awesome-icon icon="bicycle"/>
+                Numéro de série : 
+                <span class="bold">{{ bikeInformations.serial_number }}
+                </span>
+            </li>
+            <br>
+            <li>
+                <font-awesome-icon icon="parking"/>
+                En ordre : 
+                <span class="bold">{{ inOrder }}
+                </span>
+            </li>
+            <br>
+            <li>
+                <font-awesome-icon icon="lock-open"/>
+                Etat de service : 
+                <span class="bold">{{ serviceStatus }}
+                </span>
+            </li>
+            <br>
+            <li>
+                <font-awesome-icon icon="battery-three-quarters"/>
+            Niveau de batterie : 
+                <span class="bold">{{ bikeInformations.battery_level}}/{{ maxLevel }}
+                </span>
+            </li>
+            <br>
+            <li>
+                <font-awesome-icon icon="compass"/>
+                Coordonnées : 
+                <br>
+                lat : 
+                <span class="bold">{{ bikeInformations.location.coordinates[1]}}</span>
+                <br>
+                lng : 
+                <span class="bold">{{ bikeInformations.location.coordinates[0]}}
+                </span>
+            </li>
         </ul>    
         
     </div>
@@ -19,14 +52,34 @@ import axios from "axios"
 export default {
     data () {
         return {
-            maxLevel: 100
+            maxLevel: 100,
+            serviceStatus : '',
+            inOrder: ''
         }
     },
-    props: [ 'bikeInformations'],
+    props: ['bikeInformations'],
+    mounted () {
+        // this.getServiceStatus(),
+        // this.getInOrder()
+        this.getInformations()
+    },
     methods: {
-        updateBike(e) {
-            console.log(this.$route.query)
+        getInformations () {
+            if (this.bikeInformations.service_status === 1) {
+                this.serviceStatus = "FREE"
+            } else if (this.bikeInformations.service_status === 2) {
+                this.serviceStatus = "BOOKED"
+            } else {
+                this.serviceStatus = "IN USE"
+            }
+
+            if(this.bikeInformations.in_order === true) {
+                this.inOrder = "OUI"
+            } else {
+                this.inOrder = "NON"
+            }
         }
+        
     }
 }
 </script>
