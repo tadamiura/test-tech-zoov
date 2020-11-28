6274bb5d8a5f5bbd67ddc32a2dff7f125a5cd568
Miura <tadamiura@gmail.com>
2020-11-28 18:08:03 +0100

    changed id name

--- tadatoshi-test-tech/src/components/NewBike.vue
+++ tadatoshi-test-tech/src/components/NewBike.vue
@@ -1,99 +1,99 @@
 <template>
-    <div class="new-bike">
-        <form
-  id="app"
-  @submit="addNewBike"
-  method="post"
->
-	<h2>Ajouter un nouveau vélo</h2>
-  <p v-if="errors.length">
-    <b>Please correct the following error(s):</b>
-    <ul class="error-message">
-      <li v-for="error in errors" :key="error.id">{{ error }}</li>
-    </ul>
-  </p>
+	<div class="new-bike">
+		<form
+			id="bike"
+			@submit="addNewBike"
+			method="post"
+		>
+		<h2>Ajouter un nouveau vélo</h2>
+		<p v-if="errors.length">
+			<b>Please correct the following error(s):</b>
+			<ul class="error-message">
+				<li v-for="error in errors" :key="error.id">{{ error }}</li>
+			</ul>
+		</p>
 
-  <p>
-    <label for="serial_number">Serial Number</label><br>
-    <input
-      id="serial_number"
-      v-model="serial_number"
-      type="text"
-      name="serial_number"
-			step="any"
-			required
-    >
-  </p>
+		<p>
+			<label for="serial_number">Serial Number</label><br>
+			<input
+				id="serial_number"
+				v-model="serial_number"
+				type="text"
+				name="serial_number"
+				step="any"
+				required
+			>
+		</p>
 
-  <p>
-    <label for="location-lat">Location-lat</label><br>
-    <input
-      id="location-lat"
-      v-model="lat"
-      type="number"
-      name="location-lat"
-			step="any"
-			required
-    >
-  </p>
+		<p>
+			<label for="location-lat">Location-lat</label><br>
+			<input
+				id="location-lat"
+				v-model="lat"
+				type="number"
+				name="location-lat"
+				step="any"
+				required
+			>
+		</p>
 
-  <p>
-    <label for="location-lng">Location-lng</label><br>  
-    <input
-      id="location-lng"
-      v-model="lng"
-      type="text"
-      name="location-lng"
-			required
-    >
-  </p>
+		<p>
+			<label for="location-lng">Location-lng</label><br>  
+			<input
+				id="location-lng"
+				v-model="lng"
+				type="text"
+				name="location-lng"
+				required
+			>
+		</p>
 
-  <p>
-    <label for="in_order">In Order</label><br>
-    <select
-      id="in_order"
-      v-model="in_order"
-      name="in_order"
-    >
-      <option :value="true">True</option>
-      <option :value="false">False</option>
-    </select>
-  </p>
+		<p>
+			<label for="in_order">In Order</label><br>
+			<select
+				id="in_order"
+				v-model="in_order"
+				name="in_order"
+			>
+				<option :value="true">True</option>
+				<option :value="false">False</option>
+			</select>
+		</p>
 
-  <p>
-    <label for="service_status">Service status</label><br>
-    <select
-      id="service_status"
-      v-model="service_status"
-      name="service_status"
-    >
-      <option :value="1">FREE</option>
-      <option :value="2">BOOKED</option>
-      <option :value="3">IN USE</option>
-    </select>
-  </p>
+		<p>
+			<label for="service_status">Service status</label><br>
+			<select
+				id="service_status"
+				v-model="service_status"
+				name="service_status"
+			>
+				<option :value="1">FREE</option>
+				<option :value="2">BOOKED</option>
+				<option :value="3">IN USE</option>
+			</select>
+		</p>
 
-  <p>
-    <label for="battery_level">Battery Level</label><br>
-    <input
-      id="battery_level"
-      type="number"
-      v-model.number="battery_level"
-      name="battery_level"
-      min="0"
-      max="100"
-    >
-  </p>
-  
-  <p>
-    <input
-      type="submit"
-      value="Submit"
-    >
-  </p>
+		<p>
+			<label for="battery_level">Battery Level</label><br>
+			<input
+				id="battery_level"
+				type="number"
+				v-model.number="battery_level"
+				name="battery_level"
+				min="0"
+				max="100"
+			>
+		</p>
+		
+		<p>
+			<input
+				type="submit"
+				value="Submit"
+			>
+		</p>
 
-</form>
-    </div>
+		</form>
+	</div>
 </template>
 <script>
 import axios from "axios";
